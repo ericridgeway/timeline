28904b75e1c51424dd714c25bd427e7171e99bad
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-13 16:22:11 -0500

    Main.curValue

--- timeline/test/main_test.exs
+++ timeline/test/main_test.exs
@@ -11,17 +11,7 @@ defmodule TimelineTest.Main do
       |> Main.add("a3")
 
     assert main |> Main.history == ~w[a1 a2 a3]
-  end
-
-  # tmp internal
-  test "cur move" do
-    main =
-      Main.new()
-      |> Main.add("a1")
-      |> Main.add("a2")
-      |> Main.add("a3")
-
-    assert main.cur_move == 3
+    assert main |> Main.cur_value == "a3"
   end
 
   test "Undo" do
