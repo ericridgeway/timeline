621c3409ce50093b4500acefe97f170fbd31c205
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-13 15:46:13 -0500

    dry/unhardcode curMove

--- timeline/lib/timeline/main.ex
+++ timeline/lib/timeline/main.ex
@@ -3,16 +3,19 @@ defmodule Timeline.Main do
   def new() do
     %{
       me: [],
-      cur_move: 3,
+      cur_move: 0,
     }
   end
 
   def add(t, new) do
-    update_me(t, [new | t.me])
+    t
+    |> update_me([new | t.me])
+    |> update_cur_move(t.cur_move + 1)
   end
 
   def history(t), do: t.me |> Enum.reverse()
 
 
   defp update_me(t, new), do: Map.put(t, :me, new)
+  defp update_cur_move(t, new), do: Map.put(t, :cur_move, new)
 end
