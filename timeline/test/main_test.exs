8a0d0f69f27d2fac6782ef1d20cf8667fc2332fe
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-13 16:24:59 -0500

    pre-dry test

--- timeline/test/main_test.exs
+++ timeline/test/main_test.exs
@@ -3,15 +3,19 @@ defmodule TimelineTest.Main do
 
   alias Timeline.{Main}
 
-  test "add" do
-    main =
+  setup do
+    add3_main =
       Main.new()
       |> Main.add("a1")
       |> Main.add("a2")
       |> Main.add("a3")
 
-    assert main |> Main.history == ~w[a1 a2 a3]
-    assert main |> Main.cur_value == "a3"
+    %{add3_main: add3_main}
+  end
+
+  test "add", %{add3_main: add3_main} do
+    assert add3_main |> Main.history == ~w[a1 a2 a3]
+    assert add3_main |> Main.cur_value == "a3"
   end
 
   test "Undo" do
