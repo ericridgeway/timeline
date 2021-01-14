3e3497ccf40d7b9ea3a3dc68a409befc5d25abf7
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-13 19:46:52 -0500

    todo

--- test/todo.exs
+++ test/todo.exs
@@ -10,3 +10,4 @@
 # any undo/redo/left/right?
 # reverse history list with elixir [h | main] style
 # test any bad index errors from undo/redo/left/right when at end of list
+# test add a3, undo, add a3 doesn't make new timeline?
