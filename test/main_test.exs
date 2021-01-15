f7ff44a06de8e36dc0e358ee01c7ec72a17a632f
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-15 16:51:46 -0500

    Move almost everything back to begining except stubbed out tests, gonna try diff method

--- test/main_test.exs
+++ test/main_test.exs
@@ -6,96 +6,97 @@ defmodule TimelineTest.Main do
 
   setup do
     add3_main =
-      Main.new()
-      |> Main.add("a1")
-      |> Main.add("a2")
-      |> Main.add("a3")
+      nil
+      # Main.new()
+      # |> Main.add("a1")
+      # |> Main.add("a2")
+      # |> Main.add("a3")
 
     ~M{add3_main}
   end
 
   test "add", ~M{add3_main} do
-    assert add3_main |> Main.history == ~w[a1 a2 a3]
-    assert add3_main |> Main.cur_value == "a3"
+    # assert add3_main |> Main.history == ~w[a1 a2 a3]
+    # assert add3_main |> Main.cur_value == "a3"
   end
 
-  test "Undo", %{add3_main: add3_main} do
-    assert "a2" ==
-      add3_main
-      |> Main.undo
-      |> Main.cur_value
-  end
+  # test "Undo", %{add3_main: add3_main} do
+  #   assert "a2" ==
+  #     add3_main
+  #     |> Main.undo
+  #     |> Main.cur_value
+  # end
 
-  test "Redo", %{add3_main: add3_main} do
-    assert "a3" ==
-      add3_main
-      |> Main.undo
-      |> Main.redo
-      |> Main.cur_value
-  end
+  # test "Redo", %{add3_main: add3_main} do
+  #   assert "a3" ==
+  #     add3_main
+  #     |> Main.undo
+  #     |> Main.redo
+  #     |> Main.cur_value
+  # end
 
-  test "Undo edge cases", %{add3_main: add3_main} do
-    main =
-      add3_main
-      |> Main.undo
-      |> Main.undo
-      |> Main.undo
-      |> Main.add("cat")
-
-    assert main |> Main.history == ~w[cat]
-    assert Main.new |> Main.undo == Main.new
-  end
-
-  test "Redo edge cases", %{add3_main: add3_main} do
-    main =
-      add3_main
-      |> Main.undo
-      |> Main.redo
-      |> Main.redo
-
-    assert main == add3_main
-    assert Main.new |> Main.redo == Main.new
-  end
-
-  test "any_undos?" do
-    refute Main.new |> Main.any_undos?
-  end
-
-  test "any_redos?" do
-    refute Main.new |> Main.any_redos?
-  end
-
-  # test "Undo then move creates branch", ~M{add3_main} do
+  # test "Undo edge cases", %{add3_main: add3_main} do
   #   main =
   #     add3_main
   #     |> Main.undo
-  #     |> Main.add("b3")
-  #   assert main |> Main.history == ~w[a1 a2 b3]
+  #     |> Main.undo
+  #     |> Main.undo
+  #     |> Main.add("cat")
 
-  #   main =
-  #     main
-  #     |> Main.left
-  #   assert main |> Main.history == ~w[a1 a2 a3]
-
-# #     main =
-# #       main
-# #       |> Main.right
-# #     assert main |> Main.history == ~w[a1 a2 b3]
+  #   assert main |> Main.history == ~w[cat]
+  #   assert Main.new |> Main.undo == Main.new
   # end
 
-  # # tmp internal
-  # test "is_this_overwriting", ~M{add3_main} do
+  # test "Redo edge cases", %{add3_main: add3_main} do
   #   main =
   #     add3_main
   #     |> Main.undo
-  #     # |> Main.add("b3")
+  #     |> Main.redo
+  #     |> Main.redo
 
-  #   assert Main.is_this_overwriting?("b3")
-  #   refute Main.is_this_overwriting?("a3")
-  #   # NOTE oh, this is the same as any_redos?
-
-  #   # assert main |> Main.history == ~w[a1 a2 b3]
+  #   assert main == add3_main
+  #   assert Main.new |> Main.redo == Main.new
   # end
 
-  # test "left edgecase and any_lefts?" do
+  # test "any_undos?" do
+  #   refute Main.new |> Main.any_undos?
+  # end
+
+  # test "any_redos?" do
+  #   refute Main.new |> Main.any_redos?
+  # end
+
+  # # test "Undo then move creates branch", ~M{add3_main} do
+  # #   main =
+  # #     add3_main
+  # #     |> Main.undo
+  # #     |> Main.add("b3")
+  # #   assert main |> Main.history == ~w[a1 a2 b3]
+
+  # #   main =
+  # #     main
+  # #     |> Main.left
+  # #   assert main |> Main.history == ~w[a1 a2 a3]
+
+# # #     main =
+# # #       main
+# # #       |> Main.right
+# # #     assert main |> Main.history == ~w[a1 a2 b3]
+  # # end
+
+  # # # tmp internal
+  # # test "is_this_overwriting", ~M{add3_main} do
+  # #   main =
+  # #     add3_main
+  # #     |> Main.undo
+  # #     # |> Main.add("b3")
+
+  # #   assert Main.is_this_overwriting?("b3")
+  # #   refute Main.is_this_overwriting?("a3")
+  # #   # NOTE oh, this is the same as any_redos?
+
+  # #   # assert main |> Main.history == ~w[a1 a2 b3]
+  # # end
+
+  # # test "left edgecase and any_lefts?" do
 end
