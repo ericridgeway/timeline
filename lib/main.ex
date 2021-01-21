393c62ebf4804584b1e12a5ba9f9251fa3cf33fa
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-20 22:37:53 -0500

    Dry a little

--- lib/main.ex
+++ lib/main.ex
@@ -16,12 +16,6 @@ defmodule Timeline.Main do
     |> Map.put(:current_node_id, manual_id)
   end
 
-  def value(t, id) do
-    t.nodes
-    |> Enum.find(&Node.match?(&1, id))
-    |> Node.value
-  end
-
   def undo(%{current_node_id: 1}=t), do: t
   def undo(t) do
     t |> Map.put(:current_node_id, t.current_node_id - 1)
@@ -43,7 +37,11 @@ defmodule Timeline.Main do
 
   def get_node(t, id) do
     t.nodes
-    |> Enum.find(fn node -> node.id == id end)
+    |> Enum.find(&Node.match?(&1, id))
+  end
+
+  def value(t, id) do
+    get_node(t, id) |> Node.value
   end
 
   def current(t), do: t.current_node_id
