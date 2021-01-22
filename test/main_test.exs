dd72539fbe46bb671ccf5373543bc1f60991383e
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-21 20:10:03 -0500

    Clean tests for updated

--- test/main_test.exs
+++ test/main_test.exs
@@ -53,23 +53,6 @@ defmodule TimelineTest.Main do
     assert main |> Main.redo |> Main.current_node_id == 2
   end
 
-  # TODO prob delete this after doing below test
-  # test "History to current" do
-  #   main =
-  #     Main.new
-  #     |> Main.add("cat", 1)
-  #     |> Main.add("dog", 2)
-  #     |> Main.add("mouse", 3)
-  #     |> Main.undo
-  #     |> Main.add("cheese", 4)
-
-  #   expected = [Node.new("cat", 1), Node.new("dog", 2, 1), Node.new("cheese", 4, 2)]
-
-  #   assert main |> Main.history_to_current == expected
-  #   # main |> Main.history_to_current
-  #   # |> IO.inspect(label: "")
-  # end
-
   test "Add undo redo without overriding id's" do
     main =
       Main.new
@@ -84,28 +67,13 @@ defmodule TimelineTest.Main do
       |> Main.redo
       |> Main.redo
       |> Main.redo
-
-    assert main |> Main.history_to_current == ~w[cat dog mouse]
-  end
-
-  # tmp del after recursion
-  test "history_to_current with 4 steps instead of 3 (for recursion)" do
-    main =
-      Main.new
-      |> Main.add("cat")
-      |> Main.add("dog")
-      |> Main.add("mouse")
-      |> Main.undo
-      |> Main.add("cheese")
-      |> Main.undo
-      |> Main.redo
       |> Main.add("bat")
 
     assert main |> Main.history_to_current == ~w[cat dog mouse bat]
   end
 
   # test "down" do
-  # # big test above, down, should end in ~w[cat dog cheese]
+  # # big test above, down, should end in ~w[cat dog cheese] or w/e
   # # DRY the big prob
 
 #   # test "sort by creation order" do
