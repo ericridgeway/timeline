942f7f092354c0b3cef406a56327dde650681113
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-24 18:39:25 -0500

    Delete old ascii output, update lib again

--- lib/timeline/chart.ex
+++ lib/timeline/chart.ex
@@ -2,20 +2,6 @@ defmodule Timeline.Chart do
   alias Timeline.{Main, Node}
   alias Timeline.Chart.Square
 
-  def ascii_output(t) do
-    # t
-    # |> IO.inspect(label: "")
-    [] ++ [
-      Enum.reduce(t, [], fn {{_x,_y}, value}, output_list ->
-        output_list ++ ["#{value}"]
-      end)
-    ]
-    # TODO might be annoying to try to piecebypiece rewrite this when I get here next, instead maybe just restart it in the make sense way with enum rows, enum cols, like other times I've done ascii
-    # Enum.reduce(t, [], fn {pair, square}, row_list ->
-    # # TODO next here, just pass on to about-to-be-made AsciiOutput lib
-    # end)
-  end
-
   def new(main) do
     main
     |> Main.first_children
