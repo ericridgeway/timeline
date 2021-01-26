98c9b9e269d3f8a3e504b319f09ff2be7f1c1b2a
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-26 00:18:18 -0500

    Comment everything out for reference

--- lib/timeline/chart.ex
+++ lib/timeline/chart.ex
@@ -4,74 +4,74 @@ defmodule Timeline.Chart do
   alias AsciiOutput.Main, as: AsciiOutput
 
   def new(main) do
-    # main
-    # |> Main.first_children
-    # |> Enum.reduce(Map.new, fn node, map ->
-    #   id = node |> Node.id
-    #   value = node |> Node.value
-    #   x = Main.move_num(main, id)
-    #   y = 1
-    #   square = Square.new(id, value, :left)
+    main
+    |> Main.first_children
+    |> Enum.reduce(Map.new, fn node, map ->
+      id = node |> Node.id
+      value = node |> Node.value
+      x = Main.move_num(main, id)
+      y = 1
+      square = Square.new(id, value, :left)
 
-    #   Map.put(map, {x,y}, square)
-    # end)
+      Map.put(map, {x,y}, square)
+    end)
 
-    loop(%{}, nil, 1, main)
+    # loop(%{}, nil, 1, main)
   end
 
-  defp loop(t, cur_check_node, y, main, force_draw \\ false) do
-    IO.puts "*****starttt"; require InspectVars; InspectVars.inspect([t, cur_check_node, y])
-    cur_check_id = cur_check_node |> Node.id
+  # defp loop(t, cur_check_node, y, main, force_draw \\ false) do
+  #   IO.puts "*****starttt"; require InspectVars; InspectVars.inspect([t, cur_check_node, y])
+  #   cur_check_id = cur_check_node |> Node.id
 
-    if (main |> Main.size == 0) or (cur_check_node == nil and t != %{}) do
-      IO.puts "finished, return t"
-      t
-    else
-      first_child = Main.first_child(main, cur_check_id)
-      first_child_id = first_child |> Square.id
+  #   if (main |> Main.size == 0) or (cur_check_node == nil and t != %{}) do
+  #     IO.puts "finished, return t"
+  #     t
+  #   else
+  #     first_child = Main.first_child(main, cur_check_id)
+  #     first_child_id = first_child |> Square.id
 
-      if not(force_draw) and (first_child == nil or already_added?(t, first_child_id)) do
-        # tmp step b
-        if Main.any_downs?(main, cur_check_id) do
-          down_id = Main.down_id(main, cur_check_id)
-          down_node = Main.get_node(main, down_id)
-          IO.puts "any_downs? true, go down"
-          loop(t, down_node, y+1, main, true)
-        else
-          parent_node = Main.parent(main, cur_check_id)
-          parent_y = get_y(t, parent_node |> Node.id)
-          IO.puts "go parent"
-          loop(t, parent_node, parent_y, main)
-        end
-
-
-      else
-        first_children = main |> Main.first_children(cur_check_id)
-        proposed_map_adds =
-          Enum.reduce(first_children, %{}, fn proposed_node, proposed_map_adds ->
-            proposed_id = proposed_node |> Node.id
-            proposed_value = proposed_node |> Node.value
-
-            x = Main.move_num(main, proposed_id)
-            square = Square.new(proposed_id, proposed_value, :left)
-
-            Map.put(proposed_map_adds, {x,y}, square)
-          end)
-          |> IO.inspect(label: "proposed_map_adds")
-        if any_already_added?(t, proposed_map_adds) do
-          IO.puts "111111111already added?"
-          loop(t, cur_check_node, y+1, main)
-        else
-          new_t = Map.merge(t, proposed_map_adds)
-          last_child = first_children |> Enum.reverse |> hd
-          IO.puts "addProposedAndRunWithLast"; require InspectVars; InspectVars.inspect([new_t, last_child])
-          loop(new_t, last_child, y, main)
-        end
-      end
+  #     if not(force_draw) and (first_child == nil or already_added?(t, first_child_id)) do
+  #       # tmp step b
+  #       if Main.any_downs?(main, cur_check_id) do
+  #         down_id = Main.down_id(main, cur_check_id)
+  #         down_node = Main.get_node(main, down_id)
+  #         IO.puts "any_downs? true, go down"
+  #         loop(t, down_node, y+1, main, true)
+  #       else
+  #         parent_node = Main.parent(main, cur_check_id)
+  #         parent_y = get_y(t, parent_node |> Node.id)
+  #         IO.puts "go parent"
+  #         loop(t, parent_node, parent_y, main)
+  #       end
 
 
-    end
-  end
+  #     else
+  #       first_children = main |> Main.first_children(cur_check_id)
+  #       proposed_map_adds =
+  #         Enum.reduce(first_children, %{}, fn proposed_node, proposed_map_adds ->
+  #           proposed_id = proposed_node |> Node.id
+  #           proposed_value = proposed_node |> Node.value
+
+  #           x = Main.move_num(main, proposed_id)
+  #           square = Square.new(proposed_id, proposed_value, :left)
+
+  #           Map.put(proposed_map_adds, {x,y}, square)
+  #         end)
+  #         |> IO.inspect(label: "proposed_map_adds")
+  #       if any_already_added?(t, proposed_map_adds) do
+  #         IO.puts "111111111already added?"
+  #         loop(t, cur_check_node, y+1, main)
+  #       else
+  #         new_t = Map.merge(t, proposed_map_adds)
+  #         last_child = first_children |> Enum.reverse |> hd
+  #         IO.puts "addProposedAndRunWithLast"; require InspectVars; InspectVars.inspect([new_t, last_child])
+  #         loop(new_t, last_child, y, main)
+  #       end
+  #     end
+
+
+  #   end
+  # end
 
 
   def ascii_output(t) do
@@ -86,19 +86,19 @@ defmodule Timeline.Chart do
     Map.get(t, key) |> Square.value
   end
 
-  defp get_y(t, id) do
-    pair_and_square =
-      Enum.find(t, fn {pair, square} ->
-        square |> Square.id == id
-      end)
+  # defp get_y(t, id) do
+  #   pair_and_square =
+  #     Enum.find(t, fn {pair, square} ->
+  #       square |> Square.id == id
+  #     end)
 
-    if pair_and_square == nil do
-      :idk
-    else
-      {{_x, y}, _square} = pair_and_square
-      y
-    end
-  end
+  #   if pair_and_square == nil do
+  #     :idk
+  #   else
+  #     {{_x, y}, _square} = pair_and_square
+  #     y
+  #   end
+  # end
 
   defp any_already_added?(t, proposed_map_adds) do
     Enum.any?(proposed_map_adds, fn {pair, square} ->
